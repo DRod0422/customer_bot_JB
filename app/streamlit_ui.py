4ea291a3d13bb432ccbf551e842a4ab81317d5f9
DRod0422 <davrod0422@gmail.com>
2026-01-01 21:55:40 -0600

    Update streamlit_ui.py

--- app/streamlit_ui.py
+++ app/streamlit_ui.py
@@ -99,7 +99,7 @@ if "messages" not in st.session_state:
 st.markdown('<div class="jb-card">', unsafe_allow_html=True)
 
 for m in st.session_state.messages:
-    with st.chat_message(m["role", avatar=st.image("jb_avatar.jpeg"]):
+    with st.chat_message(m["role"], avatar=st.image("jb_avatar.jpeg"):
         st.markdown(m["content"])
 
 st.markdown("</div>", unsafe_allow_html=True)
